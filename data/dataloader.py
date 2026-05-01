48d1dc63aa46892acf437f7dbe784c238540d40e
adithya-prabhu-22 <adithyaprabhu79@gmail.com>
2026-05-01 23:28:04 +0530

    Implement GPT dataset and dataloader for autoregressive training

---
+++ data/dataloader.py
@@ -0,0 +1,43 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+from data.input_target_pairs import create_input_target_pairs
+
+
+class GPTDataset(Dataset):
+
+    def __init__(
+        self,
+        tokens,
+        context_length: int,
+    ):
+        self.input_ids, self.targets = create_input_target_pairs(
+            tokens=tokens,
+            context_length=context_length,
+        )
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.targets[idx]
+
+
+def create_dataloader(
+    tokens,
+    context_length: int,
+    batch_size: int,
+    shuffle: bool = True,
+):
+    dataset = GPTDataset(
+        tokens=tokens,
+        context_length=context_length,
+    )
+
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+    )
+
+    return dataloader
